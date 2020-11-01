adb3f07ffb701b75cb59ae000eb7a918f4c03b52
Ruxi Zhang <zruxi@vmware.com>
2020-11-01 10:20:45 -0800

    foreign key

--- sql_queries.py
+++ sql_queries.py
@@ -8,25 +8,20 @@ time_table_drop = "DROP TABLE IF EXISTS time_table"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplay_table (
-songplay_id SERIAL PRIMARY KEY, start_time TIMESTAMP, user_id int, level varchar, song_id varchar, 
-artist_id varchar, session_id int, location varchar, user_agent varchar);
-""")
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS user_table (
-user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);
+user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS song_table (
-song_id varchar, title varchar, artist_id varchar, year int, duration numeric);
+song_id varchar PRIMARY KEY, title varchar NOT NULL, artist_id varchar, year int, duration numeric);
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artist_table (
-artist_id varchar, name varchar, location varchar, latitude numeric, longitude numeric);
+artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude numeric, longitude numeric);
 """)
 
 time_table_create = ("""
@@ -34,11 +29,18 @@ CREATE TABLE IF NOT EXISTS time_table (
 start_time TIMESTAMP, hour int, day int, week int, month int, year int, weekday int);
 """)
 
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplay_table (
+songplay_id SERIAL PRIMARY KEY, start_time TIMESTAMP NOT NULL,
+user_id int references user_table, level varchar, song_id varchar NOT NULL references song_table,
+artist_id varchar NOT NULL references artist_table, session_id int, location varchar, user_agent varchar);
+""")
+
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplay_table (start_time, user_id, 
-level, song_id, artist_id, 
+INSERT INTO songplay_table (start_time, user_id,
+level, song_id, artist_id,
 session_id, location, user_agent) \
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
@@ -67,11 +69,11 @@ VALUES (%s, %s, %s, %s, %s, %s, %s)
 # FIND SONGS
 
 song_select = ("""
-SELECT song_table.song_id, artist_table.artist_id FROM song_table, artist_table 
+SELECT song_table.song_id, artist_table.artist_id FROM song_table, artist_table
 WHERE song_table.title = %s and artist_table.name = %s and song_table.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
